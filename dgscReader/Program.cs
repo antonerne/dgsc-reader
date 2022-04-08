9234990bc0ff5c8948b3142f61ad41482ac63679
Anton P Erne <79037362+antonerne@users.noreply.github.com>
2022-04-08 19:20:12 +0900

    Apr8-1920

--- dgscReader/Program.cs
+++ dgscReader/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using OsanScheduler.Models.Sites;
+using OsanScheduler.Models.Teams;
+using OsanScheduler.Models.Employees;
+using MongoDB.Driver;
 
+// open the mongo connection to the server
+
+// Read the team(s), finding the DFS Team, then find the DGS-C Site.
+
+// Now get the list of available Employees for the team.
+
+// get the list of excel files in the designated directory
+
+// find the employee file and process using EmployeeReader class object
+
+// process the rest of the excel files and process them to fill the employee
+// information
