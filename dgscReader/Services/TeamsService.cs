23f9970dc0615b2baea954dbe7ec41ada96c43b6
Anton P Erne <79037362+antonerne@users.noreply.github.com>
2022-04-21 16:01:16 +0900

    Apr21-1600

--- dgscReader/Services/TeamsService.cs
+++ dgscReader/Services/TeamsService.cs
@@ -10,7 +10,6 @@ namespace OsanScheduler.DgscReader.Services
 	public class TeamsService
 	{
 		private readonly IMongoCollection<Team> _teamsCollection;
-		private readonly IMongoCollection<Team2> _teams2Collection;
 
 		public TeamsService(ConnectionStrings connectionStrings)
         {
@@ -18,35 +17,30 @@ namespace OsanScheduler.DgscReader.Services
 				connectionStrings.DefaultConnection);
 			var mongoDatabase = mongoClient.GetDatabase("scheduler");
 			this._teamsCollection = mongoDatabase.GetCollection<Team>("teams");
-			this._teams2Collection = mongoDatabase.GetCollection<Team2>("teams");
         }
 
 		public async Task<List<Team>> GetAsync() =>
 			await this._teamsCollection.Find(_ => true).ToListAsync();
 
 		public async Task<Team> GetAsync(string id) =>
-			await this._teamsCollection.Find(x => x.Id == new ObjectId(id))
+			await this._teamsCollection.Find(x => x.Id == id)
 			.FirstOrDefaultAsync();
 
-		public async Task<Team2> GetByCodeAsync(string code) =>
-			await this._teams2Collection.Find(x => x.Code == code)
+		public async Task<Team> GetByCodeAsync(string code) =>
+			await this._teamsCollection.Find(x => x.Code == code)
 			.FirstOrDefaultAsync();
 
 		public async Task CreateAsync(Team newTeam) =>
 			await this._teamsCollection.InsertOneAsync(newTeam);
 
-		public async Task CreateManyAsync(Team2[] newTeams) =>
-			await this._teams2Collection.InsertManyAsync(newTeams);
+		public async Task CreateManyAsync(Team[] newTeams) =>
+			await this._teamsCollection.InsertManyAsync(newTeams);
 
-		public async Task UpdateAsync(ObjectId id, Team updatedTeam) =>
+		public async Task UpdateAsync(string id, Team updatedTeam) =>
 			await this._teamsCollection
 			.ReplaceOneAsync(x => x.Id == id, updatedTeam);
 
-		public async Task UpdateAsync(string id, Team2 updatedTeam) =>
-			await this._teams2Collection
-			.ReplaceOneAsync(x => x.Id == id, updatedTeam);
-
-		public async Task DeleteAsync(ObjectId id) =>
+		public async Task DeleteAsync(string id) =>
 			await this._teamsCollection.DeleteOneAsync(x => x.Id == id);
 	}
 }
